07af471e71e0b8a6aefa0852365e45d63fb4567e
hokagelegend2023 <136045532+hokagelegend2023@users.noreply.github.com>
2023-07-28 13:50:38 +0700

    Update menu.sh

--- update/menu.sh
+++ update/menu.sh
@@ -133,13 +133,13 @@ echo -e "$COLOR1└────────────────────�
 echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
 echo -e "$COLOR1│${NC}  $COLOR1[INFO]${NC} Check for Script updates"
 sleep 2
-wget -q -O /root/install_up.sh "https://raw.githubusercontent.com/hokagelegend2023/beta2/main" && chmod +x /root/install_up.sh
+wget -q -O /root/update_beta2.sh "https://raw.githubusercontent.com/hokagelegend2023/update/main/update_beta2" && chmod +x /root/install_up.sh
 sleep 2
-./install_up.sh
+./update_beta2.sh
 sleep 5
 rm /root/install_up.sh
 rm /opt/.ver
-version_up=$( curl -sS https://raw.githubusercontent.com/hokagelegend2023/ipmini/main/version)
+version_up=$(curl -sS https://raw.githubusercontent.com/hokagelegend2023/ipmini/main/version)
 echo "$version_up" > /opt/.ver
 echo -e "$COLOR1│${NC}  $COLOR1[INFO]${NC} Successfully Up To Date!"
 echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
@@ -160,7 +160,7 @@ upminutes=`uptime -p | awk '{print $4,$5}' | cut -d , -f1`
 uptimecek=`uptime -p | awk '{print $6,$7}' | cut -d , -f1`
 cekup=`uptime -p | grep -ow "day"`
 IPVPS=$(curl -s ipinfo.io/ip )
-serverV=$( curl -sS https://raw.githubusercontent.com/hokagelegend2023/ipmini/main/version)
+serverV=$(curl -sS https://raw.githubusercontent.com/hokagelegend2023/ipmini/main/version)
 if [ "$Isadmin" = "ON" ]; then
 uis="${GREEN}Premium User$NC"
 else
